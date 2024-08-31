f634cd999b50d8e5d79ba300b3c77c959d856892
wcuadrosuniandes <w.cuadrosr@uniandes.edu.co>
2024-08-31 09:04:51 -0500

    Se agrega logica de asociaciones

--- src/recetas/recetas.module.ts
+++ src/recetas/recetas.module.ts
@@ -3,12 +3,13 @@ import { RecetasService } from './recetas.service';
 import { RecetasController } from './recetas.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Receta } from './entities/receta.entity';
+import { Producto } from 'src/productos/entities/producto.entity';
 
 @Module({
   controllers: [RecetasController],
   providers: [RecetasService],
   imports: [
-    TypeOrmModule.forFeature([ Receta ])
+    TypeOrmModule.forFeature([ Receta, Producto ])
   ]
 })
 export class RecetasModule {}
