cdebab8b94da4e4737464ee2ab65829d1d9d447f
Alejandra Uniandes <ma.ninog12@uniandes.edu.co>
2024-09-03 19:28:46 -0500

    Pruebas del API en Postman-Categoria

--- src/categorias/dto/create-categoria.dto.ts
+++ src/categorias/dto/create-categoria.dto.ts
@@ -1,13 +1,10 @@
 import { IsString, MinLength } from 'class-validator';
 
 export class CreateCategoriaDto {
-
-    public id:string;
     @IsString({message: 'El campo nombre debe ser un string' })
     @MinLength(1,{message: 'El campo nombre no debe estar vacío'}) 
     public nombre: string;
 
-
-    @IsString()
+    @IsString({message: 'El campo nombre debe ser un string' })
     public descripcion: string;
 }
