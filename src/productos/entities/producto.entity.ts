53ea8b9ffa3195aa82a445209ecf1c17b359b5b3
wcuadrosuniandes <w.cuadrosr@uniandes.edu.co>
2024-09-26 21:30:22 -0500

    Se agrega grapqhl a recetas y recetas productos

--- src/productos/entities/producto.entity.ts
+++ src/productos/entities/producto.entity.ts
@@ -3,24 +3,33 @@ import { Cultura } from '../../culturas/entities/cultura.entity';
 import { Categoria } from '../../categorias/entities/categoria.entity';
 import { Receta } from '../../recetas/entities/receta.entity';
 import { Column, Entity, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Field, ObjectType } from '@nestjs/graphql';
 
+@ObjectType()
 @Entity()
 export class Producto {
+
+    @Field()
     @PrimaryGeneratedColumn('uuid')
     id: string;
     
+    @Field()
     @Column()
     nombre: string;
  
+    @Field()
     @Column()
     descripcion: string;
     
+    @Field()
     @Column()
     historia: string;
     
+    @Field()
     @ManyToOne(() => Categoria, categoria => categoria.productos)
     categoria: string;
 
+    @Field(() => [Receta])
     @ManyToMany(() => Receta, (receta) => receta.productos)
     recetas: Receta[];
 
--- src/recetas/entities/receta.entity.ts
+++ src/recetas/entities/receta.entity.ts
@@ -1,31 +1,42 @@
 import { Cultura } from "../../culturas/entities/cultura.entity";
 import { Producto } from '../../productos/entities/producto.entity'; 
 import { Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Field, ObjectType } from '@nestjs/graphql';
 
+@ObjectType()
 @Entity()
 export class Receta {
+
+    @Field()
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @Field()
     @Column('text')
     nombre: string;
 
+    @Field()
     @Column('text')
     descripcion: string;
 
+    @Field()
     @Column('text')
     foto: string;
 
+    @Field()
     @Column('text')
     proceso: string;
 
+    @Field()
     @Column('text')
     video: string;
 
+
     @ManyToOne(() => Cultura, (cultura) => cultura.recetas, { onDelete: 'CASCADE' })
     cultura: Cultura;
-    
+
+    @Field(() => [Producto])
     @ManyToMany(() => Producto, (producto) => producto.recetas)
-    @JoinTable()  // Define la tabla intermedia
+    @JoinTable() 
     productos: Producto[];
 }
